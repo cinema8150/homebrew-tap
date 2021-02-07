3d1e34a5ba384317992d49a8e37880a8b022aca1
cheng.yin <cheng_yin@jiliguala.com>
2021-02-07 18:26:04 +0800

    edit install cmd

--- gitlab.rb
+++ gitlab.rb
@@ -5,21 +5,9 @@ class Gitlab < Formula
     version = '0.1.0'
     sha256 "7da9e21b7365030ca649288243ede1dca33326436615123c51220cf5d530f221"
     # license ""
-    
-    depends_on "go" => :build
-  
+      
     def install
-      system "go", "build", "-o", bin/"gitlab"
-      prefix.install_metafiles
-      # Install bash completion
-      output = Utils.safe_popen_read("#{bin}/gitlab", "completion", "bash")
-      (bash_completion/"gitlab").write output
-      # Install zsh completion
-      output = Utils.safe_popen_read("#{bin}/gitlab", "completion", "zsh")
-      (zsh_completion/"_gitlab").write output
-      # Install fish completion
-      output = Utils.safe_popen_read("#{bin}/gitlab", "completion", "fish")
-      (fish_completion/"gitlab.fish").write output
+      bin.install "gitlab"
     end
   
   end
