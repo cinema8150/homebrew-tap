d799eb911645db5ab6a4531276205c0d1224737c
cheng.yin <cheng_yin@jiliguala.com>
2021-02-07 18:53:21 +0800

    edit .rb

--- gitlab.rb
+++ gitlab.rb
@@ -1,7 +1,7 @@
 class Gitlab < Formula
     desc "gitlab bate version."
     homepage "https://github.com/cinema8150/gitlab"
-    url "https://github.com/cinema8150/gitlab/archive/v0.1.0.tar.gz"
+    url "https://github.com/cinema8150/gitlab/releases/download/v0.1.0/gitlab"
     version = '0.1.0'
     sha256 "7e740d7f5ef349f124eb827cbd35cfd0a42a8d3a9e683d31f88183e1aa771512"
     # license ""
