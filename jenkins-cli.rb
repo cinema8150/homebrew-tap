93f158da87df3e868341d2f1ed8cad8ba9f0b908
cinema8150 <cinema8150@hotmail.com>
2022-05-19 18:15:32 +0800

    add jenkins-cli

--- jenkins-cli.rb
+++ jenkins-cli.rb
@@ -1,24 +1,23 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class JenkinsBuild < Formula
-    desc ""
-    homepage ""
-    url "https://github.com/cinema8150/jenkins-cli/archive/refs/tags/0.4.0.tar.gz"
-    sha256 "b2ff8ba463e30896520ce4dd096c3b5ae3208db2bb443e2efe2e3bbbe9b7aa91"
-    license ""
-    
-    def install
+class JenkinsCli < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/cinema8150/jenkins-cli/archive/refs/tags/0.3.0.tar.gz"
+  sha256 "c7ac0315786fd39e9e50404e7d5ca53a7a7abf1cbc7f5edbbcec67dc50eb2c91"
+  license ""
 
-      mkdir_p libexec/"bin"
-      mv 'jenkins-cli.jar', libexec/"bin"
-  
-      bin.install "jenkins-cli"
-  
-      system "echo", "If unable to access jarfile XXX, run:
-      jenkins-cli config --jarfile #{libexec}/bin/jenkins-cli.jar"
-  
-    end
-  
+  def install
+
+    mkdir_p libexec/"bin"
+    mv 'jenkins-cli.jar', libexec/"bin"
+
+    bin.install "jenkins-cli"
+
+    system "echo", "If unable to access jarfile XXX, run:
+    jenkins-cli config --jarfile #{libexec}/bin/jenkins-cli.jar"
+
   end
   
+end
