0a9b3a88b83e4e62e6170d2226d5d29f646a6c5d
cinema8150 <cinema8150@hotmail.com>
2022-07-08 18:42:35 +0800

    support skip specified function

--- publish.rb
+++ publish.rb
@@ -4,8 +4,8 @@
 class Publish < Formula
   desc ""
   homepage ""
-  url "https://github.com/cinema8150/publish/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "7a005205a7042bc32b4f0b82f967f72f153c0db729a34141781cb0ec0f73b36d"
+  url "https://github.com/cinema8150/publish/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "f7f1ca3d171b4978e3c5c820eef5b1d2f912c17bd0733e30cb635a55408e3a71"
   license ""
 
   def install
